e3fa7f6b23292c7533520eeee5905b55cce1f138
play452 <oscarlin@edu.schools.us.kg>
2024-11-04 18:50:02 +0000

    signup system works

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const path = require('path');
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.post('/signup-confirm', (req, res) => {
+    const apiKey = process.env.PTERO;
+    const apiUrl = 'https://panel_link/api/application/users';
 
-app.post('/', (req, res) => {
-    const { username, email, password } = req.body;
-    res.send('Signup successful!');
+    axios({
+        method: 'post',
+        url: apiUrl,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + apiKey
+        },
+        data: {
+            email: req.body.email,
+            username: req.body.username,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name
+        }
+    })
+    .then(response => {
+        console.log(response.data);
+        res.send('User created successfully');
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(500).send('Error creating user');
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Veazur Client is running on http://localhost:${port}`);
 });
